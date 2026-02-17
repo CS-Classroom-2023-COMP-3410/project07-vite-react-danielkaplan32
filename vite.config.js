3236ac28028f14c77a28b6b3f4dc15f3a4f566a9
Daniel K <daniel.kaplan32@du.edu>
2026-02-16 20:34:48 -0700

    Add base to vite config for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/project07-vite-react-danielkaplan32/',
   server: {
     port: 5173, // You can change this if needed
   },
