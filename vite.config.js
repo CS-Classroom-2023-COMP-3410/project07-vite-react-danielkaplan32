2ad1584043ada59cbc9ab0acefee8e6fe079f7ff
Daniel K <daniel.kaplan32@du.edu>
2026-02-16 21:04:11 -0700

    Remove base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/project07-vite-react-danielkaplan32/',
   server: {
     port: 5173, // You can change this if needed
   },
